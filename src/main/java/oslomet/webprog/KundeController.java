7de6b48c3a4b3ae556fad5cd9f231d0d5746650d
Martin Lilleeng Sætra <martinl@oslomet.no>
2021-03-25 13:55:06 +0100

    Initial commit

---
+++ src/main/java/oslomet/webprog/KundeController.java
@@ -0,0 +1,30 @@
+package oslomet.webprog;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class KundeController {
+
+    @Autowired
+    KundeRepository rep;
+
+    @GetMapping("/lagre")
+    public void lagreKunde(Kunde innKunde){
+        rep.lagreKunde(innKunde);
+    }
+
+    @GetMapping("/hentAlle")
+    public List<Kunde> hentAlle(){
+        return rep.hentAlleKunder();
+    }
+
+    @GetMapping("/slettAlle")
+    public void slettAlle(){
+        rep.slettAlleKunder();
+    }
+}
+
